ff3f919a3dc1e61035b5d9780700daa2d04853e1
Jason Ozias <jason.g.ozias@gmail.com>
2023-09-18 08:50:58 -0400

    Fixed tarpaulin issue, removed unknown lint

--- src/lib.rs
+++ src/lib.rs
@@ -291,11 +291,7 @@
 // nightly only lints
 #![cfg_attr(
     all(msrv, nightly),
-    deny(
-        ambiguous_glob_imports,
-        invalid_reference_casting,
-        unknown_diagnostic_attributes
-    )
+    deny(ambiguous_glob_imports, invalid_reference_casting)
 )]
 // nightly or beta only lints
 #![cfg_attr(
