f9d7a2296c638f655d5e803dc5ffaa792bf8915e
Jason Ozias <jason.g.ozias@gmail.com>
2025-10-01 10:28:22 -0400

    Expose get_effective_level

--- src/utils.rs
+++ src/utils.rs
@@ -11,8 +11,9 @@ use tracing_subscriber::fmt::format::FmtSpan;
 
 use crate::TracingConfig;
 
+/// Determine the effective logging level based on quiet and verbosity settings.
 #[cfg(debug_assertions)]
-pub(crate) fn get_effective_level(quiet: u8, verbosity: u8) -> Level {
+pub fn get_effective_level(quiet: u8, verbosity: u8) -> Level {
     if verbosity > 0 {
         match verbosity {
             1 => Level::DEBUG,
@@ -28,6 +29,7 @@ pub(crate) fn get_effective_level(quiet: u8, verbosity: u8) -> Level {
     }
 }
 
+/// Determine the effective logging level based on quiet and verbosity settings.
 #[cfg(not(debug_assertions))]
 pub(crate) fn get_effective_level(_quiet: u8, verbosity: u8) -> Level {
     match verbosity {
